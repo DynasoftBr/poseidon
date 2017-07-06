8ffe512b199f68be15d367a9b181eb7ffb260a2c
Leandro-Albano <leandro.albanoe@hotmail.com>
2017-07-06 20:58:48 -0300

    chenges in validate msg

--- src/api/v1/data/validation/validate-msg.ts
+++ src/api/v1/data/validation/validate-msg.ts
@@ -1,17 +1,4 @@
-import * as util from "util";
-
-const ValidateMsgs = {
-    invalidEntityProperty: "The property '%s' is not valid for entity type '%s'.",
-    requiredProperty: "The property '%s' is required.",
-    maxLength: "The maximum character allowed to property '%s' is %d.",
-    linkedPropertyWithoutId: "The linked property '%s' does not have an '_id' field.",
-    invalidLinkedPropertyId: "Can't find an entity of type '%s' with the id '%s'.",
-    invalidDataType: "Property '%s' has an invalid data type, was expected '%s'."
-};
-
 export class ValidateMsg {
-    constructor(public message: string, public property: string) {
+    constructor(public message: string, public property: string, public etName: string) {
     }
-
-    
 }
