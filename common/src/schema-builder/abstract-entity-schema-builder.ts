d4c267b63599a9ccc301f17b15aa9a352a8e9ba9
Leandro Albano <leandro.albanoe@hotmail.com>
2018-06-24 15:56:54 +0100

    adding some comments and minor refactories

--- common/src/schema-builder/abstract-entity-schema-builder.ts
+++ common/src/schema-builder/abstract-entity-schema-builder.ts
@@ -21,26 +21,33 @@ export class AbstractEntitySchamBuilder extends AbstractSchamaBuilderStrategy {
     async build(rootSchema: FluentSchemaBuilder, validation: Validation): Promise<FluentSchemaBuilder> {
 
         const ref = validation.ref;
-        const propSchema = new FluentSchemaBuilder({}).$ref("#/definitions/" + ref.name);
+        const definitions: any = (rootSchema.getSchema().definitions);
 
-        const definitions = (<any>rootSchema.getSchema().definitions);
-
+        // If the root schema has a definition for this entity type, just return a refernce to that definition.
         if (definitions && definitions[ref.name])
-            return propSchema;
+            return new FluentSchemaBuilder({}).$ref("#/definitions/" + ref.name);
 
+        // If the root schema doesn't have a definition for this entity type, we build one.
         else {
             const definition = new SchemaBuilder().object();
-            rootSchema.definitions(ref.name, definition);
 
-            definition.additionalProperties(false);
+            // First of all we add the new definition to the root schema to allow recursive calls know that it already exist
+            rootSchema.definitions(ref.name, definition);
 
             const abstractEtType = await this.entityTypeRepository.findById(ref._id);
 
-            abstractEtType.props.forEach(async (absProp) => {
+            // The abstract also can not have additional properties.
+            definition.additionalProperties(false);
+
+            const propsLength = abstractEtType.props.length;
+
+            // Iterate abstract entity's properties and build the schema for each one.
+            for (let idx = 0; idx < propsLength; idx++) {
+                const absProp = abstractEtType.props[idx];
                 const schema = await this.entitySchemaBuilder.buildSchemaValidation(rootSchema, absProp.validation);
 
                 definition.prop(absProp.name, schema, absProp.validation.required);
-            });
+            }
         }
     }
 }
