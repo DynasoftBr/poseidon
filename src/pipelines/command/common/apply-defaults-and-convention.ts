4a8550bd6013846649bdee5915158588c65c93a8
Leandro Albano <leandro.albanoe@hotmail.com>
2019-10-19 13:34:38 +0100

    fixing type problems

--- src/pipelines/command/common/apply-defaults-and-convention.ts
+++ src/pipelines/command/common/apply-defaults-and-convention.ts
@@ -4,8 +4,8 @@ import { NextPipelineItem } from "../command-pipeline-item";
 
 export async function applyDefaultsAndConvention<T extends IConcreteEntity>(
     request: ICommandRequest<T>,
-    next?: NextPipelineItem) {
-
+    next?: NextPipelineItem
+) {
     const { entity, entityType } = request;
 
     entityType.props.forEach(p => {
@@ -13,11 +13,11 @@ export async function applyDefaultsAndConvention<T extends IConcreteEntity>(
 
         // Apply defaults just on insert operations.
         if (request.operation == "insert" && validation.default && !entity[name])
-            entity[name] = parseDefault(validation.default, validation.type);
+            entity[name as keyof T] = parseDefault(validation.default, validation.type);
 
         // Apply convention.
         if (validation.convention && entity[name])
-            entity[name] = toConvention(entity[name], validation.convention);
+            entity[name as keyof T] = toConvention(entity[name], validation.convention) as any;
     });
 
     next(request);
@@ -30,12 +30,10 @@ export async function applyDefaultsAndConvention<T extends IConcreteEntity>(
  * @return A string with the convention applied.
  */
 function toConvention(propVal: string, convention: PropertyConventions): string {
-    if (convention === PropertyConventions.lowerCase)
-        return propVal.toLowerCase();
-    else if (convention === PropertyConventions.uppercase)
-        return propVal.toUpperCase();
+    if (convention === PropertyConventions.lowerCase) return propVal.toLowerCase();
+    else if (convention === PropertyConventions.uppercase) return propVal.toUpperCase();
     else if (convention === PropertyConventions.capitalizeFirstLetter) {
-        return propVal.replace(/\w\S*/g, function (txt) {
+        return propVal.replace(/\w\S*/g, function(txt) {
             return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
         });
     }
@@ -50,20 +48,13 @@ function toConvention(propVal: string, convention: PropertyConventions): string 
 function parseDefault(defaultVal: string, type: PropertyTypes): any {
     const parsed: string = handleConstants(defaultVal);
 
-    if (type === PropertyTypes.string || type === PropertyTypes.enum)
-        return parsed;
-    else if (type === PropertyTypes.boolean)
-        return new Boolean(parsed);
-    else if (type === PropertyTypes.array)
-        return [parsed];
-    else if (type === PropertyTypes.dateTime)
-        return new Date(parsed);
-    else if (type === PropertyTypes.number)
-        return Number.parseFloat(parsed);
-    else if (type === PropertyTypes.int)
-        return Number.parseInt(parsed);
-    else
-        throw Error("");
+    if (type === PropertyTypes.string || type === PropertyTypes.enum) return parsed;
+    else if (type === PropertyTypes.boolean) return new Boolean(parsed);
+    else if (type === PropertyTypes.array) return [parsed];
+    else if (type === PropertyTypes.dateTime) return new Date(parsed);
+    else if (type === PropertyTypes.number) return Number.parseFloat(parsed);
+    else if (type === PropertyTypes.int) return Number.parseInt(parsed);
+    else throw Error("");
 }
 
 /**
@@ -74,12 +65,10 @@ function parseDefault(defaultVal: string, type: PropertyTypes): any {
 function handleConstants(text: string) {
     const matches = text.match(/\[\[(\w*)\]\]/);
 
-    if (!matches)
-        return text;
+    if (!matches) return text;
 
     matches.forEach((key, idx) => {
-        if (key === "[[NOW]]")
-            text = text.replace(key, new Date().toISOString());
+        if (key === "[[NOW]]") text = text.replace(key, new Date().toISOString());
     });
 
     return text;
